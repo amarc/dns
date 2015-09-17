903ec0b1ffdf0c02b5d373ba169f3bb9ad37ae4d
Amar Cosic <amar.cosic@gmail.com>
2015-09-17 20:40:18 +0200

    Adding sportsport.rs

---
+++ zones/sportsport.rs
@@ -0,0 +1,5 @@
++sportsport.rs:88.198.18.204:300
++www.sportsport.rs:88.198.18.204:300
+Cvideo.sportsport.rs:sportsport.rs:300
+Cwap.sportsport.rs:sportsport.rs:300
+Cm.sportsport.rs:sportsport.rs:300
